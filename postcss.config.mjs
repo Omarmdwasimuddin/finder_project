67d6720fe650b2ffb877def6d1bc60e51981f507
Muhammad Wasim Uddin Omar <mdwasimu015@gmail.com>
2025-04-26 16:33:59 +0600

    Fix problem inshallah

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-const config = {
-  plugins: [
-    "@tailwindcss/postcss",
-    "autoprefixer",
-  ],
-};
-
-export default config;
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // No config path needed for v4
+    autoprefixer: {},
+  }
+}
